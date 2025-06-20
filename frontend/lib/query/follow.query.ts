649f627d3bf5d52169ebbb5fc6e9076bc2e38a1f
miinhho <mjang6988@gmail.com>
2025-06-20 16:41:09 +0900

    feat: add follow query

---
+++ frontend/lib/query/follow.query.ts
@@ -0,0 +1,16 @@
+import { apiPost } from '@/lib/axios';
+import { FollowStatus } from '@/lib/types/status.types';
+import { useMutation } from '@tanstack/react-query';
+
+interface FollowData {
+  status: FollowStatus;
+  targetId: string;
+}
+export const followQueryFn = async (targetId: string) => {
+  const response = await apiPost<FollowData>(`follow/${targetId}`);
+  return response.data;
+};
+export const useFollowQuery = () =>
+  useMutation({
+    mutationFn: (targetId: string) => followQueryFn(targetId),
+  });
